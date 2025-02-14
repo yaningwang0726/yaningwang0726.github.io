422bf40783d973c7b619769a1a8523817bbe7f18
yaningwang0726 <13870675659@163.com>
2025-02-14 16:41:34 +0800

    Site updated: 2025-02-14 16:41:34

--- js/music.js
+++ js/music.js
@@ -6,7 +6,7 @@ const ap = new APlayer({
 	{
         name: '亢奋',
         artist: '艾热 AIR',
-        url: 'https://m10.music.126.net/20250214162723/d0d869f2e4972315ef4eadc2cb82e56f/yyaac/obj/wonDkMOGw6XDiTHCmMOi/14051943566/98b6/9bb9/6769/95ed8affacc68e74318ca85116770d8b.m4a?vuutv=zOOhd/kCEF+1/SiU+2SnDxcLu6gKIJK5uU2eU7AGbAk7eS/3MHnCnWDHg3fU3UeTXy6MB2g4yJGA15YJ4UDoNQDue8yPV/bQd0OS8Obc5Dk=',
+        url: 'http://music.163.com/song/media/outer/url?id=1407594567.mp3',
         cover: 'https://p1.music.126.net/AKwTdOfUFg7pBtUSyB9z-w==/109951164524519969.jpg?param=34y34',
     }, 
 	]

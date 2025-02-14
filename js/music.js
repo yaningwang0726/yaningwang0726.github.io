9253bce2234b6b38a8dae88b744a48fbd7be4c52
yaningwang0726 <13870675659@163.com>
2025-02-14 21:05:17 +0800

    Site updated: 2025-02-14 21:05:17

--- js/music.js
+++ js/music.js
@@ -9,5 +9,11 @@ const ap = new APlayer({
         url: 'http://music.163.com/song/media/outer/url?id=1407594567.mp3',
         cover: 'https://p1.music.126.net/AKwTdOfUFg7pBtUSyB9z-w==/109951164524519969.jpg?param=34y34',
     }, 
+    {
+        name: '杜鹃',
+        artist: '李佳隆',
+        url: 'http://music.163.com/song/media/outer/url?id=2064487982.mp3',
+        cover: 'https://p1.music.126.net/PhBNYTX3FDNoVmONk6W28w==/109951168743738121.jpg?param=130y130',
+    }, 
 	]
 });

6b857debc715bbd4485be3c417844aa24cdeb6ab
yaningwang0726 <13870675659@163.com>
2025-07-08 23:56:51 +0800

    Site updated: 2025-07-08 23:56:50

--- js/music.js
+++ js/music.js
@@ -10,10 +10,16 @@ const ap = new APlayer({
         cover: 'https://p1.music.126.net/AKwTdOfUFg7pBtUSyB9z-w==/109951164524519969.jpg?param=34y34',
     }, 
     {
+        name: '鼻塞',
+        artist: '陈一豪Clear / INDEcompany',
+        url: 'http://music.163.com/song/media/outer/url?id=2714340376.mp3',
+        cover: 'https://p2.music.126.net/POtte5sgkL1WN6S9yap0kQ==/109951171367489486.jpg?param=34y34', 
+    }, 
+    {
         name: '喜鹊',
         artist: 'Capper / LEGGO',
         url: 'http://music.163.com/song/media/outer/url?id=2610629046.mp3',
         cover: 'https://p2.music.126.net/GX6tMnRMfobKiy3NJrD4og==/109951169846410884.jpg?param=130y130',
-    }, 
+    },
 	]
 });
